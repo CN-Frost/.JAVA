b52888a8e360883a94c4241742358e164781b571
BuildTools <unconfigured@null.spigotmc.org>
2021-01-24 10:51:14 +0800

    给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
    连续递增的子序列 可以由两个下标 l 和 r（l < r）确定，如果对于每个 l <= i < r，都有 nums[i] < nums[i + 1] ，那么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

--- src/FindLengthOfLCIS.java
+++ src/FindLengthOfLCIS.java
@@ -1,7 +1,7 @@
 public class FindLengthOfLCIS {
     public static int findLengthOfLCIS(int[] nums) {
-        int Ans=1,temp=1;
         if(nums.length==0)return 0;
+        int Ans=1,temp=1;
         for(int i=0;i<nums.length-1;i++)
         {
             if(nums[i]>=nums[i+1]){
